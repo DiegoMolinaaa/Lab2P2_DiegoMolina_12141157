ba5ea26aaa6938488931925299a5573d3b62bac6
DiegoMolinaaa <diego.molina0801@gmail.com>
2022-01-28 14:04:25 -0600

    Se agrega el scanner y la ArrayList de tipo Animal

--- src/lab2p2_diegomolina_12141157/Lab2P2_DiegoMolina_12141157.java
+++ src/lab2p2_diegomolina_12141157/Lab2P2_DiegoMolina_12141157.java
@@ -5,6 +5,9 @@
  */
 package lab2p2_diegomolina_12141157;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author diego
@@ -14,8 +17,11 @@ public class Lab2P2_DiegoMolina_12141157 {
     /**
      * @param args the command line arguments
      */
+    public static Scanner lea = new Scanner(System.in);
+    public static ArrayList<Animales> registro = new ArrayList();
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
